c72c5ad7c538f2bd9c9c21c7e9699877594ffbc8
Mary Le <maaaryle@gmail.com>
2014-10-05 21:59:27 -0700

    working mailgun api; was able to send anon message

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,18 @@
 var express = require('express');
-var Mailgun = require('mailgun-js');
 var router = express.Router();
-
-var api_key = 'key-5fb6c139d9f06bffde33425f20ad98f7';
-var domain = 'sandbox0975063c170c400ea508a04bfcd3d141.mailgun.org';
-var from_who = 'anon@email.com';
+var m = require('mailgun');
 
 
 
 router.post('/sendMail', function(req, res){
+    var mg = new m.Mailgun('key-5fb6c139d9f06bffde33425f20ad98f7');
 
-    var mailgun = new Mailgun({apiKey: 'key-5fb6c139d9f06bffde33425f20ad98f7', domain: 'https://sandbox0975063c170c400ea508a04bfcd3d141.mailgun.org'});
-
-    var data = {
-        //Specify email data
-        from: "anon@email.com",
-        //The email to contact
-        to: req.body.theirEmail,
-        //Subject and text data
-        subject: 'Hello from ANON',
-        html: req.body.messages
-    }
-    console.log(mailgun.messages());
-    //Invokes the method to send emails given the above data with the helper library
-    mailgun.messages().send(data, function (err, body) {
-        //If there is an error, render the error page
-        if (err) {
-            res.render('error', { error : err});
-            console.log("got an error: ", err);
-        }
-        //Else we can greet and leave
-        else {
-            //Here "submitted.jade" is the view file for this landing page
-            //We pass the variable "email" from the url parameter in an object rendered by Jade
-            res.redirect('/');
-            console.log(body);
-        }
-    });
-
+    mg.sendText("anon@anon.com", req.body.theirEmail, "Message from ANON", req.body.messages);
     console.log(req.body.theirEmail);
     console.log(req.body.messages);
+    res.redirect('/');
+
+
 });
 /* GET home page. */
 router.get('/', function(req, res) {
