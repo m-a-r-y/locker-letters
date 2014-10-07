e0e04b70df36adbd7af2b6fdb1e55b49e041194f
Mary Le <maaaryle@gmail.com>
2014-10-07 16:40:12 -0700

    update file and title to aMail

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var m = require('mailgun');
 router.post('/sendMail', function(req, res){
     var mg = new m.Mailgun('key-5fb6c139d9f06bffde33425f20ad98f7');
 
-    mg.sendText("no-reply@anon.com", req.body.theirEmail, "A Message From Mamihlapinatapai", req.body.messages + "\n \n \n \n \n ------- \n This message was brought to you by Mamihlapinatapai via http://bit.ly/1t01rjc \n Please do not respond back to this email.");
+    mg.sendText("no-reply@amail.com", req.body.theirEmail, "A Message From aMail", req.body.messages + "\n \n \n \n \n ------- \n This message was brought to you by aMail via http://bit.ly/1t01rjc \n Please do not respond back to this email.");
     console.log(req.body.theirEmail);
     console.log(req.body.messages);
     res.redirect('/');
